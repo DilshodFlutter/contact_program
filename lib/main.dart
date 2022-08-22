181e016ae2dbcb47ed241adf26376694eb28f1df
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-08-22 11:11:28 +0500

    splashScren/appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:contact_program/src/ui/contact_screen.dart';
+import 'package:contact_program/src/ui/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ContactScreen(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/src/ui/splash_screen.dart
+++ lib/src/ui/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:contact_program/src/block/contact_block.dart';
+import 'package:contact_program/src/model/contact_model.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,6 +12,71 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        actions: [
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Icon(
+              Icons.person_add_alt_1,
+              size: 30.0,
+              color: Colors.blue,
+            ),
+          ),
+          SizedBox(
+            width: 30.0,
+          ),
+        ],
+      ),
+      body: StreamBuilder<List<ContactModel>>(
+        stream: contactBlock.getContactData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<ContactModel> data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin:
+                      const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              data[index].name,
+                            ),
+                          ),
+                          Text(
+                            data[index].number,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
